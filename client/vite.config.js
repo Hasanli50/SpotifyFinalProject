4bbb3c6474ff80b71d89914f26b42c9172ac7b35
Ayla <ayla.hasanli50@gmail.com>
2025-02-23 20:28:08 +0400

    Configure Vite configuration settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,36 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  assetsInclude: ['**/*.woff2', '**/*.woff', '**/*.ttf', '**/*.otf'],
+
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles')
+    }
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/scss/variables.scss";
+          @import "@/assets/scss/mixins.scss";
+        `
+      }
+    }
+  },
+
+  build: {
+    assetsDir: 'assets', 
+    rollupOptions: {
+      external: ['**/*.woff2', '**/*.woff', '**/*.ttf', '**/*.otf']
+    }
+  }
 })
