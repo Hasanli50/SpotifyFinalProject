1e23d96656c1d222696f52040b418f0795ab450b
Ayla <ayla.hasanli50@gmail.com>
2025-02-23 20:56:08 +0400

    fix problem

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,36 +1,29 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  
-  assetsInclude: ['**/*.woff2', '**/*.woff', '**/*.ttf', '**/*.otf'],
+
+  assetsInclude: ["**/*.woff2", "**/*.woff", "**/*.ttf", "**/*.otf"],
 
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@styles': path.resolve(__dirname, 'src/styles')
-    }
-  },
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "@/assets/scss/variables.scss";
-          @import "@/assets/scss/mixins.scss";
-        `
-      }
-    }
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
   },
 
   build: {
-    assetsDir: 'assets', 
+    assetsDir: "assets",
     rollupOptions: {
-      external: ['**/*.woff2', '**/*.woff', '**/*.ttf', '**/*.otf']
-    }
-  }
-})
+      external: ["**/*.woff2", "**/*.woff", "**/*.ttf", "**/*.otf"],
+    },
+  },
+
+  // Add this block to exclude dependencies
+  optimizeDeps: {
+    exclude: ['some-package-name']  // Replace 'some-package-name' with the name of the package you're having trouble with
+  },
+});
