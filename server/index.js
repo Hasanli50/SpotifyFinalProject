40e9207e5ee8d8f553779a7932c2c0f1c907c7d8
Ayla <ayla.hasanli50@gmail.com>
2025-02-24 21:00:20 +0400

    change client side deploy url

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(
   cors({
     origin:
       process.env.APP_BASE_URL ||
-      "https://spotify-final-project-6s59.vercel.app",
+      "https://spotify-final-project-three.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
