a5dfa40f99096c9ba7f295efdd007c3f644bda0c
Ayla <ayla.hasanli50@gmail.com>
2025-03-15 22:14:09 +0400

    fix yup import problem

--- client/src/schema/loginSchema.js
+++ client/src/schema/loginSchema.js
@@ -1,12 +1,12 @@
-import * as Yup from "yup/es";
+import * as yup from "yup";
 
 
-export const loginSchema = Yup.object().shape({
-  username: Yup.string()
+export const loginSchema = yup.object().shape({
+  username: yup.string()
     .min(3, "Username must be at least 3 characters long.")
     .required("Username is required")
     .trim(),
-  password: Yup.string()
+  password: yup.string()
     .min(6, "Password must be at least 6 characters long.")
     .matches(/\d/, "Password must contain at least one number.")
     .matches(/[A-Z]/, "Password must contain at least one uppercase letter.")
